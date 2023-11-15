04f05be7fac7b7c0adcac702ee73aa6296951be7
Islam-Glitch <islmslm999@gmail.com>
2023-11-15 23:20:40 +0200

    PSV-Studio

--- Register.h
+++ Register.h
@@ -1,3 +1,5 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
 #ifndef ___VOLEMACHINE_REGISTER_H
 #define ___VOLEMACHINE_REGISTER_H
 #include <string>
