ff634d377bee2e1fc5bc7ba4d8521562933f1bfd
إبراهيم حسن محمد حسن <ibrahimhassannamr@gmail.com>
2023-11-15 21:08:25 +0200

    add the vole machine header file

--- VoleMachine.h
+++ VoleMachine.h
@@ -1,22 +1,26 @@
-// BigReal.h
-#ifndef VoleMachine_H
-#define VoleMachine_H
-
-#include <iostream>
-#include <string>
-using namespace std;
-
-
-
-
+#ifndef ___VOLEMACHINE_VOLEMACHINE_H
+#define ___VOLEMACHINE_VOLEMACHINE_H
+#include "Memory.h"
+#include "Register.h"
 class VoleMachine {
 private:
-    
+    Memory machineMemory;
+    Register machineRegister;
+    int counter;
+    string instruction;
 
 public:
-    
-    
-
+    VoleMachine();
+    static string decimalToHexa(int dec);
+    static int hexaToDecimal(string hexa);
+    void Fetch();
+    string Decode();
+    void Execute();
+    void runOneCycle();
+    friend class VoleMachineGUI;
+    string getMemoryContentAsString() const; // Add this line
+    string getRegisterContentAsString() const; // Add this line
+    string getDecodedContent() const;
 };
 
-#endif
+#endif //___VOLEMACHINE_VOLEMACHINE_H
