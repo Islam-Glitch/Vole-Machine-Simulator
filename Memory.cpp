f089d68be52d2b101d66f6b5256fd9a88f765fe2
إبراهيم حسن محمد حسن <ibrahimhassannamr@gmail.com>
2023-11-15 21:06:15 +0200

    edit memory class

--- Memory.cpp
+++ Memory.cpp
@@ -1,58 +1,60 @@
 #include "Memory.h"
 #include <iostream>
-#include <cstring>
 #include <fstream>
-#include <sstream>
-#include <string>
-
 using namespace std;
 
-void Memory :: load_instructions(string path) {
-
-    ifstream inputfile(path);
-      if (!inputfile.is_open()){
-        cerr << "Failed to open the file\n";
-        return;
-    }
-    
-    string line;
-    for (int it = 0; it < 256; it += 1){
-        Memory_Cells[decimal_to_hexa(it)] = "00";
-    }
-
-    for (int it = 0; getline(inputfile, line); it += 2){
-        ++number_of_inst;
-        Memory_Cells[decimal_to_hexa(it)] = "";
-        Memory_Cells[decimal_to_hexa(it)] += line[0];
-        Memory_Cells[decimal_to_hexa(it)] += line[1];
-        Memory_Cells[decimal_to_hexa(it + 1)] = "";
-        Memory_Cells[decimal_to_hexa(it + 1)] += line[2];
-        Memory_Cells[decimal_to_hexa(it + 1)] += line[3];
-    }
-
-    for (auto val : Memory_Cells){
-        cout << val.first << ' ' << val.second << endl;
-    }
-    cout << number_of_inst << endl;
+Memory::Memory() {
+    for (int i{0}; i < 256; i++)
+        Memory_Cells[decimalToHexa(i)] = "00";
 }
 
-string Memory :: decimal_to_hexa(int dec){
+void Memory :: load_instructions(const string& path) {
+    string line;
+
+    ifstream input(path);
+    if (!input.is_open()){
+        cout << "Failed to open the file\n";
+        return;
+    }
+
+
+    for (int i{0}; getline(input, line); i += 2){
+        number_of_instructions += 2;
+        Memory_Cells[decimalToHexa(i)] = "";
+        Memory_Cells[decimalToHexa(i)] += line[0];
+        Memory_Cells[decimalToHexa(i)] += line[1];
+        Memory_Cells[decimalToHexa(i + 1)] = "";
+        Memory_Cells[decimalToHexa(i + 1)] += line[2];
+        Memory_Cells[decimalToHexa(i + 1)] += line[3];
+    }
+}
+
+string Memory :: decimalToHexa(const int& dec){
     int x = dec / 16;
-    string result = "";
-    if (x <= 9){
-        result += (char)x + 48;
-    }
-    else{
-        x -= 10;
-        result += (char)x + 65;
-    }
+    string result;
+    if(x <= 9) result += (char)(x + 48);
+    else x -= 10, result += (char)(x + 65);
+
     x = dec % 16;
-    if (x <= 9){
-        result += (char)x + 48;
-    }
-    else{
-        x -= 10;
-        result += (char)x + 65;
-    }
+
+    if (x <= 9) result += (char)(x + 48);
+    else x -= 10, result += (char)(x + 65);
+
     return result;
+}
+
+void Memory::clearMemory() {
+    number_of_instructions = 0;
+    for (int i{0}; i < 256; i++)
+        Memory_Cells[decimalToHexa(i)] = "00";
+}
+
+
+string Memory::getContentAsString() const {
+    // Iterate through Memory_Cells and format the content as a string
+    string content;
+    for (const auto &pair : Memory_Cells) {
+        content += pair.first + ": " + pair.second + "\n";
+    }
+    return content;
 }
